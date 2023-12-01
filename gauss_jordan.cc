550c4590d78e1cec9a8db46e820e9c6406bdfafc
diegocamargo55555 <diegocamargo55555@gmail.com>
2023-12-01 04:19:30 -0300

    gauss jordan completo

--- gauss_jordan.cc
+++ gauss_jordan.cc
@@ -1,17 +1,42 @@
-#include <iostream>
-#include "funcao.h"
+#include<iostream>
 using namespace std;
-
-int main()
+int main() 
 {
-    fill_m(m1);
+    int i,j,k,n; 
+    float a[10][10],b,x[10];
 
-    for (int i = 0; i < 3; i++)
+    //printf("\ndigite o tamanho da matriz: ");
+    
+    cout << "\ndigite o tamanho da matriz: ";
+    scanf("%d",&n);
+    //
+    for(i=1; i<=n; i++)
     {
-        m1[0][i] = m1[0][i] / m1[0][0];
+        for(j=1; j<=(n+1); j++) 
+        {
+            cout << "A[" << i << ", " << j << " ]=";
+            cin >> a[i][j];
+        }
     }
-    
-    
-
-    puts_matriz(m1);
+    for(j=1; j<=n; j++) 
+    {
+        for(i=1; i<=n; i++) 
+        {
+            if(i!=j) 
+            {
+                b=a[i][j]/a[j][j];
+                for(k=1; k<=n+1; k++) 
+                { 
+                    a[i][k]=a[i][k]-b*a[j][k];
+                }
+            }
+        }
+    }
+    cout<<"\nA solução é:\n";
+    for(i=1; i<=n; i++) 
+    {
+        x[i]=a[i][n+1]/a[i][i];
+        cout<<"x"<<i << "="<<x[i]<<" ";
+    }
+    cout << endl;
 }
